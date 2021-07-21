e206f00c13c561838c1928be3adc60e119ba189d
devbackend1.neofruition <dev.backend1.neofruition@gmail.com>
2021-07-21 15:31:32 +0530

    Testing By Kenil

--- src/screens/layouts/Header.js
+++ src/screens/layouts/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
         <div className="ui fixed menu">
             <div className="ui center aligned container">
                 <h2>Products Management</h2>
+                <h2>Products Management</h2>
             </div>
         </div>
     );
